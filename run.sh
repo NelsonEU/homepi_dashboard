bd215b7378f9cd2607dc83ad070306ea65d5d84b
RPI - Home <a.etienne@outlook.com>
2025-11-27 13:42:38 +0100

    Use env for build

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd /home/arnaud/dashboard_api
+# Load prod env vars
+source /home/arnaud/dashboard-api/dashboard.env # Env file path
+
+# Move to project root
+cd "$DASHBOARD_API_ROOT_DIR"
 
 # Start uvicorn using the venv python explicitly
-exec /home/arnaud/dashboard_api/.venv/bin/python -m uvicorn app.main:app --host 0.0.0.0 --port 8000
+exec "$DASHBOARD_API_ROOT_DIR/.venv/bin/python" -m uvicorn app.main:app --host 0.0.0.0 --port 8000
